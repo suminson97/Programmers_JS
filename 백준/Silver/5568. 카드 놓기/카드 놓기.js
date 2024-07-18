d47da57c0a3a68622d7de56a423c77d965dd9dd6
suminson97 <126536402+suminson97@users.noreply.github.com>
2024-07-19 00:43:27 +0900

    [Silver IV] Title: 카드 놓기, Time: 124 ms, Memory: 10008 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.js
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.js
@@ -2,34 +2,31 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const inputLine = input.map((v) => v.trim());
 const [n, k, ...cardNum] = inputLine;
-const numBag = [];
+const set = new Set();
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
-    if (+k === 2) {
-      if (i === j) {
-        continue;
-      }
-      numBag.push(cardNum[i] + cardNum[j]);
-      continue;
+    if (+k === 2 && i !== j) {
+      set.add(cardNum[i] + cardNum[j]);
     }
     for (let o = 0; o < n; o++) {
-      if (+k === 3) {
-        if (i === j || i === o || j === o) {
-          continue;
-        }
-        numBag.push(cardNum[i] + cardNum[j] + cardNum[o]);
-        continue;
+      if (+k === 3 && i !== j && i !== o && o !== j) {
+        set.add(cardNum[i] + cardNum[j] + cardNum[o]);
       }
       for (let z = 0; z < n; z++) {
-        if (i === j || i === o || i === z || j === o || j === z || o === z) {
-          continue;
+        if (
+          +k === 4 &&
+          i !== j &&
+          i !== o &&
+          i !== z &&
+          j !== o &&
+          j !== z &&
+          o !== z
+        ) {
+          set.add(cardNum[i] + cardNum[j] + cardNum[o] + cardNum[z]);
         }
-        numBag.push(cardNum[i] + cardNum[j] + cardNum[o] + cardNum[z]);
       }
     }
   }
 }
-const newNumBag = numBag.filter((v, i) => {
-  return numBag.indexOf(v) === i;
-});
-console.log(newNumBag.length);
+const answer = set.size
+console.log(answer);
