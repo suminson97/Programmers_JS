15a4be979cbe689cd6fd1dcc5180e2589655902e
suminson97 <126536402+suminson97@users.noreply.github.com>
2025-03-05 22:10:39 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,31 @@
+function solution(numer1, denom1, numer2, denom2) {
+    let answer = [];
+    let 분자합= numer1*denom2+numer2*denom1
+    let 분모합= denom1*denom2
+    let 최대공약수 = gcd(분자합,분모합)
+   if(최대공약수===1){
+       answer.push(분자합,분모합)
+   }else answer.push(분자합/최대공약수,분모합/최대공약수)
+    
+    
+    function gcd(a,b){
+        
+     let c = 0; 
+  while(b !== 0){ 
+    c = a % b; 
+    a = b; 
+    b = c; 
+  } 
+        return a
+}
+    
+    return answer;
+}
+
+//  분수의 덧셈을 하는방법 
+// 1.두 분모의 최소공배수를 구한후 덧셈을 하고 약분을 한다
+// 2.두수의 곱하기로 분모를 맞춘후 덧셈을 하고 약분을 한다
+
+// 1 경우: 2/4+3/4 = 5/4 
+// 2 경우: 1/2+3+4 인경우 4/8+6/8 =10/8 =5/4
+ // 2번의 경우로 약분을 하기 전상태까지 만들기.
