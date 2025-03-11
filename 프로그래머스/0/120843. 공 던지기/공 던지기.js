0a1e3d4d18fae8f7e531f93c99ee00b450558bef
suminson97 <126536402+suminson97@users.noreply.github.com>
2025-03-11 18:48:17 +0900

    [level 0] Title: 공 던지기, Time: 0.50 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,14 +1,9 @@
+
+2
+3
+4
+5
 function solution(numbers, k) {
-    let num = 0;
-    let answer =0
-   while (k){
-      answer= numbers[num]
-       num+=2
-       if(num>numbers.length){
-           num=num-numbers.length
-       }
-       k--
-   }
-    console.log(numbers.length)
-    return answer;
+
+    return numbers[((2 * (k -1))) % numbers.length];
 }
