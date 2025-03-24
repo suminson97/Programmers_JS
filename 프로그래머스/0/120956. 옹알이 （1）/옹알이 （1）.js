0378e5d3f5f2ef35e4b80e5983754c9bceb6a8d7
suminson97 <126536402+suminson97@users.noreply.github.com>
2025-03-24 23:45:20 +0900

    [level 0] Title: 옹알이 (1), Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -27,8 +27,4 @@ function solution(babbling) {
     }
     return answer;
 }
-// 2words => ['ye','ma']
-// 3words => ['woo','aya']
-// babbling을 순회하며 [i]요소가 1글자면 pass , 2글자면 2words 일치하는지 확인
-// 3글자이상이면 순서대로 2글자 자른후  일치하는지 확인, 3글자 자른후 3words 일치하는지 확인 
-// 만약 2글자가 2words에 포함되는데 뒤에 글자가 한글자다 ? pass
+
